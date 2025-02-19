822b640802ec0f73f8eb492af9fd8ca6075c2e03
André <andre_miguel_costa@hotmail.com>
2025-02-19 11:22:35 +0100

    feat: impl app

---
+++ labo1/hps_gpio/soft/src/hps.c
@@ -0,0 +1,25 @@
+#include "hps.h"
+
+void hps_setup()
+{
+	GPIO_REG(GPIO_PORT(GPIO_LED), GPIO_SWPORTA_DDR) |=
+		1 << GPIO_PORT_BIT(GPIO_LED);
+
+	GPIO_REG(GPIO_PORT(GPIO_BTN), GPIO_SWPORTA_DDR) &=
+		~(1 << GPIO_PORT_BIT(GPIO_BTN));
+}
+bool hps_btn_pressed()
+{
+	return GPIO_REG(GPIO_PORT(GPIO_BTN), GPIO_SWPORTA_DR) &
+	       GPIO_PORT_BIT(GPIO_BTN);
+}
+void hps_led_set_on()
+{
+	GPIO_REG(GPIO_PORT(GPIO_LED), GPIO_SWPORTA_DR) |=
+		1 << GPIO_PORT_BIT(GPIO_LED);
+}
+void hps_led_set_off()
+{
+	GPIO_REG(GPIO_PORT(GPIO_LED), GPIO_SWPORTA_DR) &=
+		~(1 << GPIO_PORT_BIT(GPIO_LED));
+}
---
+++ labo1/hps_gpio/soft/src/timer.h
@@ -0,0 +1,29 @@
+#ifndef TIMER_H
+#define TIMER_H
+
+#include <stdint.h>
+#include <stdbool.h>
+
+#define TIMER0_BASE_ADDR 0xFFD00000
+
+#define TIMER0_REG(_x_)                 \
+	*(volatile uint32_t             \
+		  *)(TIMER0_BASE_ADDR + \
+		     _x_) // _x_ is an offset with respect to the base address
+
+#define OSC1_CLOCK	      25e6
+#define TIMER_LOAD_COUNT_REG  0x0
+#define TIMER_CURRENT_VAL_REG 0x4
+#define TIMER_CTRL_REG	      0x8
+#define TIMER_EOI_REG	      0xC
+#define TIMER_INT_STAT_REG    0x10
+
+#define TIMER_IRQ_LINE_NO     201
+
+void timer_setup(uint32_t timer_duration);
+void timer_irq_clear(void);
+void timer_enable(void);
+void timer_disable(void);
+bool timer_elapsed(void);
+
+#endif
