610e0139a16ad9aa46e27eae59d7dbeb54c033f5
André <andre_miguel_costa@hotmail.com>
2025-02-19 13:19:08 +0100

    fix: use free running timer

--- labo1/hps_gpio/soft/src/timer.c
+++ labo1/hps_gpio/soft/src/timer.c
@@ -1,21 +1,20 @@
 #include "timer.h"
-void timer_setup(uint32_t timer_duration)
+void timer_setup(void)
 {
 	//Disable the timer by writing a 0 to the timer1 enable bit (timer1_enable) of the
 	//timer1controlreg register.
 	TIMER0_REG(TIMER_CTRL_REG) &= ~0x1;
 
+	TIMER0_REG(TIMER_LOAD_COUNT_REG) = 0xFFFFFFFF;
+
 	//Program the timer mode—user-defined count or free-running—by writing a 0 or 1, respectively, to
 	//the timer1 mode bit (timer1_mode) of the timer1controlreg register.
-	TIMER0_REG(TIMER_CTRL_REG) |= 0x2;
+	TIMER0_REG(TIMER_CTRL_REG) &= ~0x2;
 
 	//Set the interrupt mask as either masked or not masked by writing a 1 or 0, respectively, to the
 	//timer1_interrupt_mask bit of the timer1controlreg register.
-	TIMER0_REG(TIMER_CTRL_REG) &= ~0x4;
+	TIMER0_REG(TIMER_CTRL_REG) |= 0x4;
 
-	//Load the timer counter value into the timer1loadcount register.
-	TIMER0_REG(TIMER_LOAD_COUNT_REG) =
-		(uint32_t)(OSC1_CLOCK * timer_duration);
 }
 
 void timer_irq_clear(void)
@@ -32,7 +31,11 @@ void timer_disable(void)
 {
 	TIMER0_REG(TIMER_CTRL_REG) &= ~0x1;
 }
-bool timer_elapsed(void)
+uint32_t timer_value(void)
 {
-	return TIMER0_REG(TIMER_CURRENT_VAL_REG) == 0;
+	return TIMER0_REG(TIMER_CURRENT_VAL_REG);
+}
+uint32_t timer_ms(void)
+{
+	return  TIMER0_REG(TIMER_CURRENT_VAL_REG) / (OSC1_CLOCK / 1e3);
 }
