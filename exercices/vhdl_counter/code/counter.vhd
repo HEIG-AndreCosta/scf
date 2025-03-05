ddb621bffca395c2bdb0330576a01d5f9b7cf539
André <andre_miguel_costa@hotmail.com>
2025-03-05 11:45:16 +0100

    feat: add counter process

--- exercices/vhdl_counter/code/counter.vhd
+++ exercices/vhdl_counter/code/counter.vhd
@@ -17,3 +17,30 @@ port (
     nbdecr_o    : out std_logic_vector(SIZE-1 downto 0)
 );
 end counter;
+
+architecture behave of counter is
+
+    signal value_s : unsigned(SIZE-1 downto 0);
+    signal nbincr_s : unsigned(SIZE-1 downto 0);
+    signal nbdecr_s : unsigned(SIZE-1 downto 0);
+
+begin
+    process (clk_i, rst_i)
+    begin
+        if rst_i = '1' then
+            value_s <= to_unsigned(0);
+            nbincr_s <= to_unsigned(0);
+            nbdecr_s <= to_unsigned(0);
+        elsif rising_edge(clk_i) then
+            if enable_i = '1' then
+                if up_nDown_i = '1' then
+                    value_s <= value_s + 1;
+                    nbincr_s <= nbincr_s + 1;
+                else 
+                    value_s <= value_s - 1;
+                    nbdecr_s <= nb_decr_s + 1;
+                end if;
+            end if;
+        end if;
+    end process;
+end architecture;
