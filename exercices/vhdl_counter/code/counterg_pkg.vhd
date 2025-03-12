6b7669b48b9892a4655e14ac231055ea9ba8da5e
André <andre_miguel_costa@hotmail.com>
2025-03-12 10:41:55 +0100

    feat: impl counter using gaisler methodology

--- exercices/vhdl_counter/code/counterg_pkg.vhd
+++ exercices/vhdl_counter/code/counterg_pkg.vhd
@@ -1,14 +1,20 @@
 library ieee;
 use ieee.std_logic_1164.all;
 
+
 package counterg_pkg is
-
-      type counter_in_type is record
-      	-- TODO : Complete
-      end record;
-
-      type counter_out_type is record
-      	-- TODO : Complete
-      end record;
+  
+  type counter_in_type is record        
+    enable    : std_logic;
+    up_nDown  : std_logic;
+    equal_val : std_logic_vector(7 downto 0);
+  end record;
+  
+  type counter_out_type is record
+    equals    : std_logic;
+    value     : std_logic_vector(7 downto 0);
+    nbincr    : std_logic_vector(7 downto 0);
+    nbdecr    : std_logic_vector(7 downto 0);
+  end record;
 
 end package;
